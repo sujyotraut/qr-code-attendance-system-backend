9b94ca992b63e5282a88cfa5d3d32aeabd55d49a
Sujyot <70472979+SujyotRaut@users.noreply.github.com>
2023-06-09 12:00:43 +0530

    Fix 2

--- src/middleware/pagination.middleware.ts
+++ src/middleware/pagination.middleware.ts
@@ -5,7 +5,10 @@ function pagination(defaultPageSize: number = 10) {
     query('_skip').customSanitizer((_, { req }) => {
       const _page = Math.abs(Number(req.query?._page || '0'));
       const _limit = Math.abs(Number(req.query?._limit || defaultPageSize.toString()));
+      console.log(_page);
+      console.log(_limit);
       const _skip = (_page - 1) * _limit;
+      console.log(_skip);
       return _skip;
     }),
     query('_limit').customSanitizer((value) => Math.abs(Number(value)) || defaultPageSize),
